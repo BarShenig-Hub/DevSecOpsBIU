346f0de47ef7972a016790a4781cda19a4607eba
Bar Shenig <Barshenig@gmail.com>
2025-12-25 19:21:13 +0200

    Commit the Alice project to repository

--- Python/alice_book.py
+++ Python/alice_book.py
@@ -3,14 +3,16 @@
 import re
 from collections import Counter
 
-alice_book = open(r"C:\Users\Bar Shenig\OneDrive\Desktop\pg11.txt", mode='r', encoding='utf8')
-
-def reading_content():
+def reading_content(alice_book):
     read_alice_book = alice_book.read()
     return read_alice_book
 
 def clean_text_and_return_list(read_alice_book):
+    # Remove all characters that are NOT letters (A-Z, a-z) or whitespace
+    # Replaces punctuation, numbers, and special characters with a space
     clean_text = re.sub(r"[^A-Za-z\s]", " ", read_alice_book)
+    # Replace multiple consecutive whitespace characters with a single space
+    # Then remove leading/trailing whitespace with strip()
     clean_text = re.sub(r"\s+", " ", clean_text).strip()
     clean_text = clean_text.lower()
     clean_text = [word for word in clean_text.split() if len(word) > 1]
@@ -21,9 +23,15 @@ def word_frequency(words_list):
     sorted_word_count = dict(word_count.most_common())
     return sorted_word_count
 
-read_alice_book = reading_content()
-clean_text = clean_text_and_return_list(read_alice_book)
-freq_dict = word_frequency(clean_text)
 
-print(freq_dict)
-print("The word Alice is presented", freq_dict['alice'], "times")
+def do_all():
+    alice_book = open('alice.txt', mode='r', encoding='utf8')
+    read_alice_book = reading_content(alice_book)
+    clean_text = clean_text_and_return_list(read_alice_book)
+    freq_dict = word_frequency(clean_text)
+    first_key = next(iter(freq_dict))
+    first_value = freq_dict[first_key]
+    print(f'Most common word is "{first_key}", count: {first_value}')
+    print("The word Alice is presented", freq_dict['alice'], "times")
+
+do_all()
