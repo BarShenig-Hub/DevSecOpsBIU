d8db68bdd79c52f10fc62cb540d5185321ca1519
Bar Shenig <Barshenig@gmail.com>
2025-12-25 21:28:21 +0200

    Commit exceptions.py to repository

--- Python/exceptions.py
+++ Python/exceptions.py
@@ -1,7 +1,22 @@
-try:
-    num = 5/2
-    f1 = open("moshe")
-except FileNotFoundError:
-    print('No such file')
-except ZeroDivisionError:
-    print('do not divide by zero')
+
+def do_math(b):
+    num = 10 / b
+    return num
+    
+def provide_nums():
+    for num in range(-5, 6):
+        b = num
+        print('b =', b)
+        try:
+            result = do_math(b)
+            print('result of division is', result)
+        except Exception as e:
+            print(type(e))
+            print(e)
+            print('b is', b, 'and therefore it\'s led to an exception')
+
+
+def main():
+    provide_nums()
+
+main()
